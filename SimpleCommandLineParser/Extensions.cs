f2df3b7c53b5d0060ab7c48cd998b5dc0dc3d05c
Philippe Raemy <philippe.raemy@kochind.com>
2025-04-21 06:34:50 +0200

    Remove refs to morelinq and mannex

--- SimpleCommandLineParser/Extensions.cs
+++ SimpleCommandLineParser/Extensions.cs
@@ -17,7 +17,21 @@ namespace SimpleCommandlineParser
             return split.Length != 2 ? resultFunc(split[0], split[1]) : resultFunc(split[0], string.Empty);
         }
 
-        public static KeyValuePair<TKey, TValue> AsKeyTo<TKey, TValue>(this TKey key, TValue value) 
+        public static KeyValuePair<TKey, TValue> AsKeyTo<TKey, TValue>(this TKey key, TValue value)
             => new KeyValuePair<TKey, TValue>(key, value);
+
+        public static void ForEach<T>(this IEnumerable<T> sequence, Action<T> lambda)
+        {
+            foreach (var item in sequence) lambda?.Invoke(item);
+        }
+
+        public static IEnumerable<T> Pipe<T>(this IEnumerable<T> sequence, Action<T> lambda)
+        {
+            foreach (var item in sequence)
+            {
+                lambda?.Invoke(item);
+                yield return item;
+            }
+        }
     }
 }
